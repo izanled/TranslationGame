e08530bb8207c5fd336da29753ca8b44fa4fbe2d
izanled <ss123ss4@gmail.com>
2018-10-22 18:11:29 +0900

    1. 액티비티 오류 해결 2. 재실행 시 종료 라고 되있던 문제 수정

--- app/src/main/java/com/izanled/translation/game/view/CaptureActivity.java
+++ app/src/main/java/com/izanled/translation/game/view/CaptureActivity.java
@@ -66,7 +66,7 @@ public class CaptureActivity extends AppCompatActivity {
         } else {
             Log.i("Is on?", "Turning immersive mode mode on.");
         }
-    // 몰입 모드를 꼭 적용해야 한다면 아래의 3가지 속성을 모두 적용시켜야 합니다
+        // 몰입 모드를 꼭 적용해야 한다면 아래의 3가지 속성을 모두 적용시켜야 합니다
         newUiOptions ^= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
         newUiOptions ^= View.SYSTEM_UI_FLAG_FULLSCREEN;
         newUiOptions ^= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
